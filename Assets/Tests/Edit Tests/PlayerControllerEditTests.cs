81c2c3b9591128b2c308c6f06293436e2a55e39e
vatiwot <ajonlibajernyolc@gmail.com>
2023-11-13 19:40:01 +0100

    added pickup logic test

--- Assets/Tests/Edit Tests/PlayerControllerEditTests.cs
+++ Assets/Tests/Edit Tests/PlayerControllerEditTests.cs
@@ -9,16 +9,27 @@ public class PlayerControllerEditTests
     [Test]
     public void PlayerControllerSpeedUpTest()
     {
-        //Arrange
+
         PlayerController player = new PlayerController();
+
+
+
+
+
+
+
         float initialMoveSpeed = player.moveSpeed;
+
+
+
         player.speedUp = true;
 
-        //Act
+
         player.SetSpeedUp();
+
+
         float resultMoveSpeed = player.moveSpeed;
 
-        //Assert
         Assert.AreNotEqual(initialMoveSpeed, resultMoveSpeed);
     }
 }
