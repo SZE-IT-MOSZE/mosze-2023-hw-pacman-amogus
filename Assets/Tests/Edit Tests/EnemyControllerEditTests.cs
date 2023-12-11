9f67262e75698c269d6deee072c79af07904e33b
Boda-Peter <boda.peter17@gmail.com>
2023-12-11 15:04:06 +0100

    Added comments for Edit Tests- EnemyCOntrollerEditTests.cs & PLayerControllerEditTests.cs

--- Assets/Tests/Edit Tests/EnemyControllerEditTests.cs
+++ Assets/Tests/Edit Tests/EnemyControllerEditTests.cs
@@ -1,7 +1,13 @@
 using NUnit.Framework;
 
+/// <summary>
+/// Az EnemyControllerEditTests osztály tartalmazza az EnemyController tesztjeit módosított függvényekkel.
+/// </summary>
 public class EnemyControllerEditTests
 {
+    /// <summary>
+    /// Teszteli az ellenség irányváltását.
+    /// </summary>
     [Test]
     public void EnemyDirectionChangeTest()
     {
--- Assets/Tests/Edit Tests/PlayerControllerEditTests.cs
+++ Assets/Tests/Edit Tests/PlayerControllerEditTests.cs
@@ -1,29 +1,28 @@
 using NUnit.Framework;
 
+/// <summary>
+/// A PlayerControllerEditTests osztály tartalmazza a PlayerController tesztjeit módosított függvényekkel.
+/// </summary>
 public class PlayerControllerEditTests
 {
+    /// <summary>
+    /// Teszteli a játékos sebességének növelését.
+    /// </summary>
     [Test]
     public void PlayerControllerSpeedUpTest()
     {
-
+        //Előkészítés
         PlayerController player = new PlayerController();
-
-
-
-
-
-
-
+        
+        //Tesztterületen belüli üres sorok elhagyása
         float initialMoveSpeed = player.moveSpeed;
 
-
-
+        //Cselekvés
         player.speedUp = true;
-
 
         player.SetSpeedUp();
 
-
+        //Ellenőrzés
         float resultMoveSpeed = player.moveSpeed;
 
         Assert.AreNotEqual(initialMoveSpeed, resultMoveSpeed);
